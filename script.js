0d8b39a75133f8b1f656a3933f7d3c8a21f13afd
Daria Smirnova <darya.smirn1@yandex.ru>
2022-03-28 17:48:48 +0300

    Выполнила дз урока №3

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+'use strict';
+
 let num = 266219;
 
 //переменная для результирующего значения
@@ -14,4 +16,38 @@ console.log(multiplication);
 let exponentiation = multiplication ** 3;
 
 console.log(exponentiation.toString().substring(0, 2));
+
+//урок 3
+//задание 1
+const lang = 'en';
+
+//a
+if (lang === 'ru') {
+	console.log('Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье');	
+} else if (lang === 'en') {
+	console.log('Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday');	
+}
+
+//b
+switch (lang) {
+	case 'ru':
+		console.log('Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье');
+		break;
+	case 'en':
+		console.log('Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday');
+		break;
+}
+
+//c
+const week = [
+	'ru', ['Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье'],
+	'en', ['Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday']	
+];
+console.log(week[week.indexOf(lang)+1][0]);
+
+//задание 2
+const namePerson = 'Александр';
+
+namePerson === 'Артем' ? console.log('директор') :
+	namePerson === 'Александр' ? console.log('преподаватель') : console.log('студент');
 
