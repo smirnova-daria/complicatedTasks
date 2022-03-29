49a6e2e0f6be28986f39478b99858ce4d9140bd1
Daria Smirnova <darya.smirn1@yandex.ru>
2022-03-29 18:02:07 +0300

    Выполнила дз урока №4

--- script.js
+++ script.js
@@ -51,3 +51,21 @@ const namePerson = 'Александр';
 namePerson === 'Артем' ? console.log('директор') :
 	namePerson === 'Александр' ? console.log('преподаватель') : console.log('студент');
 
+
+//урок 4
+
+const func = function (arg) {
+	if (typeof arg !== 'string') {
+		return 'Аргумент не строка';
+	}
+	if (arg.trim().length > 30) {
+		return arg.trim().substring(0, 30) + '...';
+	}
+	return arg.trim();
+};
+
+console.log(func(123));
+console.log(func(true));
+console.log(func('     Очень длинная строка, количество символов без учета пробелов более 30 знаков        '));
+console.log(func('маленькая строка'));
+console.log(func('                  маленькая строка и пробелы             '));
