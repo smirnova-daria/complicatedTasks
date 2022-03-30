beb6d17221f2f05e8e0af632833641c43bcbeb68
Daria Smirnova <darya.smirn1@yandex.ru>
2022-03-30 18:30:46 +0300

    Выполнила дз урока №5

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
-let num = 266219;
+'use strict';
 
-//переменная для результирующего значения
-let multiplication = 1;
+//задание 1
 
-//цикл для произведения цифр числа num
-while (num) {
-	multiplication *= (num % 10);
-	num = Math.floor(num / 10);
+const arr = ['2506', '6532', '895325', '45268', '754', '9654', '4365'];
+
+for (let i = 0; i < arr.length; i++) {
+	if (arr[i][0] === '2' || arr[i][0] === '4') {
+		console.log(arr[i]);		
+	}
 }
 
-console.log(multiplication);
+//задание 2
 
-let exponentiation = multiplication ** 3;
+for (let i = 2; i <= 100; i++) {
+	let dividerCount = 0; 
 
-console.log(exponentiation.toString().substring(0, 2));
+	for (let k = 2; k < i; k++) {		
+		if (i % k === 0) {
+			dividerCount++;
+		}
+	}
 
+	if (dividerCount === 0) {
+		console.log(i + ' Делители этого числа: 1 и ' + i);	
+	}
+}
