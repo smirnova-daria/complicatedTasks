2805585928f5432fb152c4e2d11f91023adbde28
Daria Smirnova <darya.smirn1@yandex.ru>
2022-04-05 14:22:15 +0300

    Выполнила дз урока №9

--- script.js
+++ script.js
@@ -1,18 +1,60 @@
 'use strict';
 
-const week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
+const divFirst = document.querySelector('.first');
+const divSecond = document.querySelector('.second');
 
-const body = document.querySelector('body');
-const today = new Intl.DateTimeFormat('ru-RU', {weekday: 'long'}).format(new Date());
+const printTime = function () {
+	const time = new Date();
 
-week.forEach(function (value) {
-	const paragraph = document.createElement('p');
-	paragraph.textContent = value;
-	if (value === 'Суббота' || value === 'Воскресенье') {
-		paragraph.classList.add('italic');
+	const formatter1 = new Intl.DateTimeFormat("ru");
+
+	const formatter2 = new Intl.DateTimeFormat("ru", {
+		weekday: "long",
+		year: "numeric",
+		month: "long",
+		day: "numeric"
+	});
+
+	const formatter3 = new Intl.DateTimeFormat("ru", {
+		hour: "numeric",
+		minute: "numeric",
+		second: "numeric"
+	});
+	const hours = time.getHours();
+	const minutes = time.getMinutes();
+	const seconds = time.getSeconds();
+
+	let hoursDesc = '';
+	let minutesDesc = '';
+	let secondsDesc = '';
+
+	if (hours === 1 || hours === 21) {
+		hoursDesc = ' час ';
+	} else if ((hours >= 2 && hours <= 4) || (hours >= 22 && hours <= 24)) {
+		hoursDesc = ' часа ';
+	} else {
+		hoursDesc = ' часов ';
 	}
-	if (value.toLowerCase() === today) {
-		paragraph.classList.add('bold');		
+
+	if ((minutes % 10 === 1) && (minutes != 11)) {
+		minutesDesc = ' минута ';
+	} else if ((minutes % 10 >= 2) && (minutes % 10 <= 4)) {
+		minutesDesc = ' минуты ';
+	} else {
+		minutesDesc = ' минут ';
 	}
-	body.appendChild(paragraph);
-});
+
+	if ((seconds % 10 === 1) && (seconds != 11)) {
+		secondsDesc = ' секунда ';
+	} else if ((seconds % 10 >= 2) && (seconds % 10 <= 4)) {
+		secondsDesc = ' секунды ';
+	} else {
+		secondsDesc = ' секунд ';
+	}
+
+	divFirst.textContent = 'a) Сегодня ' + formatter2.format(time)[0].toLocaleUpperCase() + formatter2.format(time).slice(1, -1) + 'ода, ' + (hours + hoursDesc + minutes + minutesDesc + seconds + secondsDesc);
+	divSecond.textContent = 'b) ' + formatter1.format(time) + ' - ' + formatter3.format(time);
+};
+
+printTime();
+setInterval(printTime, 500);
