f6c7de31cd6e3ed02ed114fff806a4cff0d65efa
Daria Smirnova <darya.smirn1@yandex.ru>
2022-04-19 19:28:12 +0300

    Выполнила пункт 6 дз урока №19

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
-let num = 266219;
+'use strict'
+let timer
+const render = () => {
+	const date = new Date()
 
-//переменная для результирующего значения
-let multiplication = 1;
+	const hours = date.getHours()
+	const greeting = (hours >= 6 && hours < 12) ? 'Доброе утро!' :
+		(hours >= 12 && hours < 18) ? 'Добрый день!' :
+		(hours >= 18 && hours < 24) ? 'Добрый вечер!' : 'Доброй ночи!';
 
-//цикл для произведения цифр числа num
-while (num) {
-	multiplication *= (num % 10);
-	num = Math.floor(num / 10);
+	const week = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота']
+	const weekDay = `Сегодня: ${week[date.getDay()]}`
+
+	const time = `Текущее время: ${date.toLocaleTimeString('en')}`
+
+	const newYear = new Date('1 january 2023')
+
+	const daysUntilNewYear = `До нового года осталось ${Math.ceil((newYear - date) / 1000 / 60 / 60 / 24)} дней`
+	document.body.innerHTML = `<p>${greeting} <hr> ${weekDay} <br> ${time} <br> ${daysUntilNewYear}</p>`
+	if (newYear - date <= 0) {
+		clearInterval(timer)
+	}
 }
 
-console.log(multiplication);
-
-let exponentiation = multiplication ** 3;
-
-console.log(exponentiation.toString().substring(0, 2));
-
+timer = setInterval(render, 1000)
