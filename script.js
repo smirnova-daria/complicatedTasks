bdc87ef0a1f66e3710695031886470489eb2020e
Daria Smirnova <darya.smirn1@yandex.ru>
2022-04-19 21:03:01 +0300

    Выполнила усложненное задание урока 19 (анимация)

--- script.js
+++ script.js
@@ -1,17 +1,42 @@
-let num = 266219;
+'use strict'
 
-//переменная для результирующего значения
-let multiplication = 1;
+const ballon1 = document.querySelector('.ballon1')
+const ballon2 = document.querySelector('.ballon2')
+const startBtn = document.querySelector('.start')
+const resetBtn = document.querySelector('.reset')
 
-//цикл для произведения цифр числа num
-while (num) {
-	multiplication *= (num % 10);
-	num = Math.floor(num / 10);
+let count = 0
+let idInterval
+let isClicked = false
+
+const flyAnimate = () => {
+	count++
+
+	idInterval = requestAnimationFrame(flyAnimate)
+
+	if (count < 400) {
+		ballon2.style.bottom = count * 1.5 + 'px'
+		ballon1.style.bottom = count + 'px'
+	} else if (count < 500) {
+		ballon1.style.bottom = count + 'px'
+	} else {
+		cancelAnimationFrame(idInterval)
+	}
 }
 
-console.log(multiplication);
+startBtn.addEventListener('click', () => {
+	isClicked = !isClicked
+	if (isClicked) {
+		flyAnimate()
+	} else {
+		cancelAnimationFrame(idInterval)
+	}
+})
 
-let exponentiation = multiplication ** 3;
-
-console.log(exponentiation.toString().substring(0, 2));
-
+resetBtn.addEventListener('click', () => {
+	cancelAnimationFrame(idInterval)
+	isClicked = false
+	count = 0
+	ballon2.style.bottom = 0 + 'px'
+	ballon1.style.bottom = 0 + 'px'
+})
